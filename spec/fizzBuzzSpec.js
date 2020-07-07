82f822d5e255a8b4b26823cf6d68ed7b153ee975
hansa-lallu <hansalallu@gmail.com>
2020-07-07 08:52:31 +0100

    divisable by 3 and 5

--- spec/fizzBuzzSpec.js
+++ spec/fizzBuzzSpec.js
@@ -2,11 +2,14 @@ const fizzBuzz = require('../src/fizzBuzz');
 
 describe('Fizzbuzz', () => {
   it('is divisable by 3', () => {
-    number = 15 
-    expect(fizzBuzz(number)).toEqual('Fizz')
+    expect(fizzBuzz(9)).toEqual('Fizz')
   });
 
   it('is divisable by 5', () => {
     expect(fizzBuzz(20)).toEqual('Buzz')
+  });
+
+  it('is divisable by 5 and 2', () => {
+    expect(fizzBuzz(15)).toEqual('FizzBuzz')
   });
 });
